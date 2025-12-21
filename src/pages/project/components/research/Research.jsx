7b196d49b0f4643e885a0c4825b6ff1c7cef8cc6
Abrahan Zarza <abrahan.azr@gmail.com>
2025-12-21 17:28:27 +0100

    feat: Implement responsive design for research templating section

--- src/pages/project/components/research/Research.jsx
+++ src/pages/project/components/research/Research.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Research.scss";
+import "./Research.responsive.scss";
 
 export default ({project}) => {
     const [isVisible, setIsVisible] = useState(false);
